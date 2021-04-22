dd4e4a52b1070c6df9b2acedffabbd7431013c3d
anto991nio <antonio.91.cardone@gmail.com>
2021-04-22 17:12:50 +0200

    array degli studenti e stampa del nome e cognome

--- script.js
+++ script.js
@@ -2,10 +2,55 @@
 // Prima parte esercizio
 // Creiamo un oggetto che descriva le proprietà dello studente
 
-var studente = {
-    "nome" : "Antonio",
-    "cognome" : "Cardone",
-    "età" : 29
+var student = {
+    "nome": "Antonio",
+    "cognome": "Cardone",
+    "età": 29
 };
 
-console.log(studente);
+
+
+// Con il ciclo for in stampiamo le proprietà dell'oggetto
+
+for (var key in student) {
+    console.log(student[key]);
+
+}
+document.getElementById("nameStudent").innerText = ("Il nome dello studente è :" + " " + student.nome)
+document.getElementById("surnameStudent").innerText = ("Il cognome dello studente è :" + " " + student.cognome)
+document.getElementById("ageStudent").innerText = ("L'età dello studente è :" + " " + student.età)
+
+// Seconda parte esercizio
+// creiamo un array che abbia al suo interno più oggetti
+
+var classRoom = [{
+    "nome": "Mario",
+    "cognome": "Rossi",
+    "età": 22
+}, {
+    "nome": "Giuseppe",
+    "cognome": "Garibaldi",
+    "età": 150
+}, {
+    "nome": "Lorenzo",
+    "cognome": "Jovanotti",
+    "età": 50
+}, {
+    "nome": "Fabio",
+    "cognome": "Cannavaro",
+    "età": 45
+}
+]
+
+//con un ciclo faccio stampare il nome e il cognome
+
+for (var i=0 ; classRoom.length ; i++){
+
+    var element = classRoom[i];
+
+
+    console.log(element.nome);
+    console.log(element.cognome);
+
+
+}
