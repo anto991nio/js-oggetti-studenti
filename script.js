9172eca6fc9053d25a85c28e3ba91a4635d37966
anto991nio <antonio.91.cardone@gmail.com>
2021-04-22 15:34:55 +0200

    Creato oggetto studente

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Prima parte esercizio
+// Creiamo un oggetto che descriva le proprietà dello studente
+
+var studente = {
+    "nome" : "Antonio",
+    "cognome" : "Cardone",
+    "età" : 29
+};
+
+console.log(studente);
